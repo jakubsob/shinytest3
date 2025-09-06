98a29b2edebb116fcb0b0dd03eab7c41c1e1a157
jakub <jakupsob@gmail.com>
2025-09-06 22:36:12 +0200

    test: :wrench: increase timeouts

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,5 +1,10 @@
 chromote::local_chrome_version(134, binary = "chrome")
 
+options(
+  shinytest2.load_timeout = 30 * 1000,
+  shinytest2.timeout = 10 * 1000
+)
+
 variant <- function() {
   ifelse(testthat::is_checking(), "check", "local")
 }
